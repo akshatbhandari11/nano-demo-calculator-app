f1a598494b201fdac4c2a28605fd6f612fad2f44
akshatbhandari11 <akshatbhandari11@gmail.com>
2023-08-20 23:11:15 +0530

    first commit xoxo

--- routes.cpp
+++ routes.cpp
@@ -2,15 +2,51 @@
 
 crow::response greet()
 {
-    return crow::response{""};
+    return crow::response{"Hello world!\n"};
 }
+
 crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    if (!input)
+    {
+        return crow::response(400, "Invalid JSON");
+    }
+
+    if (!input.has("first") || !input.has("second"))
+    {
+        return crow::response(400, "Missing 'first' or 'second' in JSON");
+    }
+
+    double first = input["first"].d();
+    double second = input["second"].d();
+    double result = first + second;
+
+    crow::json::wvalue response_json;
+    response_json["result"] = result;
+
+    return crow::response(200, response_json);
 }
+
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    if (!input)
+    {
+        return crow::response(400, "Invalid JSON");
+    }
+
+    if (!input.has("first") || !input.has("second"))
+    {
+        return crow::response(400, "Missing 'first' or 'second' in JSON");
+    }
+
+    double first = input["first"].d();
+    double second = input["second"].d();
+    double result = first - second;
+
+    crow::json::wvalue response_json;
+    response_json["result"] = result;
+
+    return crow::response(200, response_json);
 }
